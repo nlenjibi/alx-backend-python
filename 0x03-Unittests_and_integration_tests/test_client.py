457fd3f62a9c3286d83f6284239f9eca49cd32c0
NLENJIBI TIMOTHY <69208852+nlenjibi@users.noreply.github.com>
2025-11-22 09:49:28 +0000

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -107,14 +107,17 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected)
 
 
-@parameterized_class([
-    {
-        "org_payload": fixtures.org_payload,
-        "repos_payload": fixtures.repos_payload,
-        "expected_repos": fixtures.expected_repos,
-        "apache2_repos": fixtures.apache2_repos
-    }
-])
+@parameterized_class(
+    "org_payload, repos_payload, expected_repos, apache2_repos",  # Parameter names as string
+    [
+        (  # Tuple with matching order
+            fixtures.org_payload,
+            fixtures.repos_payload,
+            fixtures.expected_repos,
+            fixtures.apache2_repos
+        )
+    ]
+)
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration tests for GithubOrgClient using fixtures."""
 
